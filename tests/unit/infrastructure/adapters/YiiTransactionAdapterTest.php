d491fc0af13a6878df988dc1ab332017fc0367c9
WarLikeLaux <warlikelaux080399@mail.ru>
2025-12-31 22:35:05 +0600

    #26 - added nested transaction support to YiiTransactionAdapter

--- tests/unit/infrastructure/adapters/YiiTransactionAdapterTest.php
+++ tests/unit/infrastructure/adapters/YiiTransactionAdapterTest.php
@@ -52,6 +52,7 @@ final class YiiTransactionAdapterTest extends Unit
     {
         $transaction = $this->createMock(Transaction::class);
         $transaction->expects($this->once())->method('commit');
+        $transaction->method('getIsActive')->willReturn(true);
 
         $this->db->method('beginTransaction')->willReturn($transaction);
 
@@ -63,10 +64,118 @@ final class YiiTransactionAdapterTest extends Unit
     {
         $transaction = $this->createMock(Transaction::class);
         $transaction->expects($this->once())->method('rollBack');
+        $transaction->method('getIsActive')->willReturn(true);
 
         $this->db->method('beginTransaction')->willReturn($transaction);
 
         $this->adapter->begin();
         $this->adapter->rollback();
+    }
+
+    public function testAfterCommitCallbacksExecutedAfterCommit(): void
+    {
+        $transaction = $this->createMock(Transaction::class);
+        $transaction->method('getIsActive')->willReturn(true);
+        $this->db->method('beginTransaction')->willReturn($transaction);
+
+        $executed = false;
+        $this->adapter->begin();
+        $this->adapter->afterCommit(function () use (&$executed): void {
+            $executed = true;
+        });
+        $this->adapter->commit();
+
+        $this->assertTrue($executed);
+    }
+
+    public function testAfterCommitCallbacksNotExecutedOnRollback(): void
+    {
+        $transaction = $this->createMock(Transaction::class);
+        $transaction->method('getIsActive')->willReturn(true);
+        $this->db->method('beginTransaction')->willReturn($transaction);
+
+        $executed = false;
+        $this->adapter->begin();
+        $this->adapter->afterCommit(function () use (&$executed): void {
+            $executed = true;
+        });
+        $this->adapter->rollBack();
+
+        $this->assertFalse($executed);
+    }
+
+    public function testMultipleAfterCommitCallbacksExecutedInOrder(): void
+    {
+        $transaction = $this->createMock(Transaction::class);
+        $transaction->method('getIsActive')->willReturn(true);
+        $this->db->method('beginTransaction')->willReturn($transaction);
+
+        $order = [];
+        $this->adapter->begin();
+        $this->adapter->afterCommit(function () use (&$order): void {
+            $order[] = 'first';
+        });
+        $this->adapter->afterCommit(function () use (&$order): void {
+            $order[] = 'second';
+        });
+        $this->adapter->commit();
+
+        $this->assertSame(['first', 'second'], $order);
+    }
+
+    public function testNestedTransactionsWithSameInstance(): void
+    {
+        $transaction = $this->createMock(Transaction::class);
+        $transaction->expects($this->once())->method('commit');
+        $transaction->method('getIsActive')->willReturn(true);
+        $this->db->method('beginTransaction')->willReturn($transaction);
+
+        // Outer
+        $this->adapter->begin();
+
+        // Inner
+        $this->adapter->begin();
+        $this->adapter->commit();
+
+        // Outer commit
+        $this->adapter->commit();
+    }
+
+    public function testNestedTransactionsWithDifferentInstancesOnSameConnection(): void
+    {
+        $activeTransaction = $this->createMock(Transaction::class);
+        $activeTransaction->method('getIsActive')->willReturn(true);
+
+        $this->db->method('getTransaction')->willReturn($activeTransaction);
+
+        // Adapter 1 thinks transaction is already active, so it joins
+        $adapter1 = new YiiTransactionAdapter($this->db);
+
+        $this->db->expects($this->never())->method('beginTransaction');
+        $activeTransaction->expects($this->never())->method('commit'); // Adapter 1 is NOT owner
+
+        $adapter1->begin();
+        $adapter1->commit();
+    }
+
+    public function testRollbackResetsNestingLevel(): void
+    {
+        $transaction = $this->createMock(Transaction::class);
+        $transaction->method('getIsActive')->willReturn(true);
+        $this->db->method('beginTransaction')->willReturn($transaction);
+
+        $transaction->expects($this->once())->method('rollBack');
+
+        // Level 1
+        $this->adapter->begin();
+        // Level 2
+        $this->adapter->begin();
+
+        $this->adapter->rollBack();
+
+        // Should throw because level was reset to 0
+        $this->expectException('RuntimeException'::class);
+        $this->expectExceptionMessage('Transaction not started');
+        $this->adapter->commit();
     }
 }
