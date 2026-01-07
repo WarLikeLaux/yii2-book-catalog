21be436c493ffef5d8f66a17a3880401bfbc6faf
WarLikeLaux <warlikelaux080399@mail.ru>
2026-01-08 04:26:31 +0600

    #32 - added AutoMapper and ActiveRecordHydrator infrastructure

--- config/container/common.php
+++ config/container/common.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 use app\infrastructure\adapters\SystemClock;
 use app\infrastructure\services\YiiPsrLogger;
+use AutoMapper\AutoMapper;
+use AutoMapper\AutoMapperInterface;
 use Psr\Clock\ClockInterface;
 use Psr\Log\LoggerInterface;
 
 return static fn (array $_params) => [
     ClockInterface::class => SystemClock::class,
     LoggerInterface::class => static fn() => new YiiPsrLogger('application'),
+
+    AutoMapperInterface::class => static fn(): AutoMapperInterface => AutoMapper::create(
+        cacheDirectory: Yii::getAlias('@runtime/automapper'),
+    ),
 ];
