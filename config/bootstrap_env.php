84867dbead8d7dcdeb3d5eb35f98b57f6e43b710
WarLikeLaux <warlikelaux080399@mail.ru>
2026-01-08 06:23:47 +0600

    #32 - implemented centralized environment validation using Dotenv

---
+++ config/bootstrap_env.php
@@ -0,0 +1,20 @@
+<?php
+// phpcs:ignoreFile PSR1.Files.SideEffects.FoundWithSymbols
+
+declare(strict_types=1);
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+$dotenv->required('DB_DRIVER')->allowedValues(['mysql', 'pgsql']);
+$dotenv->required('YII_ENV')->allowedValues(['dev', 'prod', 'test']);
+$dotenv->required('YII_DEBUG')->isBoolean();
+$dotenv->required(['DB_NAME', 'DB_USER', 'DB_PASSWORD'])->notEmpty();
+$dotenv->required('SMS_API_KEY');
+
+require __DIR__ . '/env.php';
+
+defined('YII_DEBUG') or define('YII_DEBUG', env('YII_DEBUG', false));
+defined('YII_ENV') or define('YII_ENV', env('YII_ENV', 'prod'));
--- web/index.php
+++ web/index.php
@@ -3,14 +3,7 @@
 declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
-
-require __DIR__ . '/../config/env.php';
-
-defined('YII_DEBUG') or define('YII_DEBUG', env('YII_DEBUG', false));
-defined('YII_ENV') or define('YII_ENV', env('YII_ENV', 'prod'));
+require __DIR__ . '/../config/bootstrap_env.php';
 
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
