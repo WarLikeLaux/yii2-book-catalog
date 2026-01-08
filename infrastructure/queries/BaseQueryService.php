ba83beb7439bfccf8845c467dfe759412859c337
WarLikeLaux <warlikelaux080399@mail.ru>
2026-01-08 10:37:25 +0600

    #32 - added BaseQueryService for common pagination

---
+++ infrastructure/queries/BaseQueryService.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\infrastructure\queries;
+
+use app\application\common\dto\PaginationDto;
+use app\application\common\dto\QueryResult;
+use app\application\ports\PagedResultInterface;
+use AutoMapper\AutoMapperInterface;
+use yii\data\ActiveDataProvider;
+use yii\db\ActiveQueryInterface;
+use yii\db\Connection;
+
+abstract readonly class BaseQueryService
+{
+    public function __construct(
+        protected Connection $db,
+        protected AutoMapperInterface $autoMapper,
+    ) {
+    }
+
+    /**
+     * @template T of object
+     * @param class-string<T> $dtoClass
+     * @return PagedResultInterface<T>
+     */
+    protected function getPagedResult(
+        ActiveQueryInterface $query,
+        int $page,
+        int $pageSize,
+        string $dtoClass,
+    ): PagedResultInterface {
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+            'db' => $this->db,
+            'pagination' => [
+                'page' => $page - 1,
+                'pageSize' => $pageSize,
+            ],
+        ]);
+
+        $models = array_map(
+            fn(object $model): object => $this->mapToDto($model, $dtoClass),
+            $dataProvider->getModels(),
+        );
+
+        $totalCount = $dataProvider->getTotalCount();
+        $totalPages = (int)ceil($totalCount / $pageSize);
+
+        $pagination = new PaginationDto(
+            page: $page,
+            pageSize: $pageSize,
+            totalCount: $totalCount,
+            totalPages: $totalPages,
+        );
+
+        return new QueryResult(
+            models: $models,
+            totalCount: $totalCount,
+            pagination: $pagination,
+        );
+    }
+
+    /**
+     * @template T of object
+     * @param class-string<T> $targetClass
+     * @return T
+     */
+    protected function mapToDto(object $source, string $targetClass): object
+    {
+        $dto = $this->autoMapper->map($source, $targetClass);
+        assert($dto instanceof $targetClass);
+
+        return $dto;
+    }
+}
