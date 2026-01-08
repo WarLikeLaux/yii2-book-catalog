ed6628721769fa6d49f84e53b71cb655ee886fe8
WarLikeLaux <warlikelaux080399@mail.ru>
2026-01-08 15:00:19 +0600

    #32 - added check for file accessibility in UploadedFileAdapter

--- presentation/common/adapters/UploadedFileAdapter.php
+++ presentation/common/adapters/UploadedFileAdapter.php
@@ -5,12 +5,19 @@ declare(strict_types=1);
 namespace app\presentation\common\adapters;
 
 use app\domain\values\FileContent;
+use RuntimeException;
 use yii\web\UploadedFile;
 
 final readonly class UploadedFileAdapter
 {
     public function toFileContent(UploadedFile $uploadedFile): FileContent
     {
-        return FileContent::fromPath($uploadedFile->tempName, $uploadedFile->getExtension());
+        $path = $uploadedFile->tempName;
+
+        if (!file_exists($path) || !is_readable($path)) {
+            throw new RuntimeException('Temporary file is not accessible: ' . $path); // @codeCoverageIgnore
+        }
+
+        return FileContent::fromPath($path, $uploadedFile->getExtension());
     }
 }
