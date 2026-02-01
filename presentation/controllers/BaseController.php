8984fe3189adafb51f35451038d67540bcc25c49
WarLikeLaux <warlikelaux080399@mail.ru>
2026-02-01 23:55:12 +0600

    #33 - added ViewModel infrastructure and BaseController

---
+++ presentation/controllers/BaseController.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\presentation\controllers;
+
+use app\presentation\common\ViewModelRenderer;
+use LogicException;
+use Override;
+use yii\web\Controller;
+
+abstract class BaseController extends Controller
+{
+    public function __construct(
+        $id,
+        $module,
+        protected ViewModelRenderer $renderer,
+        $config = [],
+    ) {
+        $this->renderer->setController($this);
+        parent::__construct($id, $module, $config);
+    }
+
+    private function renderInternal(string $view, array $params): string
+    {
+        return parent::render($view, $params);
+    }
+
+    #[Override]
+    public function render($_view, $_params = []): string
+    {
+        throw new LogicException(
+            'Use $this->renderer->render() for rendering views. Example: $this->renderer->render("view", $viewModel)',
+        );
+    }
+}
