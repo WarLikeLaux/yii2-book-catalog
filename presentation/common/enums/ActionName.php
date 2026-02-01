273b4eb20c48ac5780757ebb9c3259269d0e909c
WarLikeLaux <warlikelaux080399@mail.ru>
2026-02-02 01:12:11 +0600

    #33 - added ActionName enum

---
+++ presentation/common/enums/ActionName.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\presentation\common\enums;
+
+enum ActionName: string
+{
+    case INDEX = 'index';
+    case VIEW = 'view';
+    case CREATE = 'create';
+    case UPDATE = 'update';
+    case DELETE = 'delete';
+    case PUBLISH = 'publish';
+    case LOGOUT = 'logout';
+    case LOGIN = 'login';
+    case SUBSCRIBE = 'subscribe';
+    case FORM = 'form';
+    case SEARCH = 'search';
+    case API = 'api';
+}
