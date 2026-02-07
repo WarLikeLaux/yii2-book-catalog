52d85469ee62e1bcfe4e99dee5eb317fb5c2a10d
WarLikeLaux <warlikelaux080399@mail.ru>
2026-02-07 11:55:12 +0600

    #34 - added BusinessRuleException, implemented fromEnum() in DomainErrorMappingRegistry

--- application/common/exceptions/DomainErrorMappingRegistry.php
+++ application/common/exceptions/DomainErrorMappingRegistry.php
@@ -5,13 +5,49 @@ declare(strict_types=1);
 namespace app\application\common\exceptions;
 
 use app\domain\exceptions\DomainErrorCode;
+use app\domain\exceptions\ErrorMapping;
+use ReflectionEnum;
 
 final class DomainErrorMappingRegistry
 {
+    private const array TYPE_MAP = [
+        'NotFound' => EntityNotFoundException::class,
+        'AlreadyExists' => AlreadyExistsException::class,
+        'OperationFailed' => OperationFailedException::class,
+        'BusinessRule' => BusinessRuleException::class,
+    ];
+
     /**
      * @var array<string, array{class-string<ApplicationException>, string|null}>
      */
     private array $mappings = [];
+
+    public static function fromEnum(): self
+    {
+        $registry = new self();
+        /** @var ReflectionEnum<DomainErrorCode> $reflection */
+        $reflection = new ReflectionEnum(DomainErrorCode::class);
+
+        foreach ($reflection->getCases() as $case) {
+            $attrs = $case->getAttributes(ErrorMapping::class);
+
+            if ($attrs === []) {
+                continue; // @codeCoverageIgnore
+            }
+
+            $mapping = $attrs[0]->newInstance();
+            $errorCode = $case->getValue();
+            assert($errorCode instanceof DomainErrorCode);
+
+            $registry->register(
+                $errorCode,
+                self::TYPE_MAP[$mapping->type->name],
+                $mapping->field,
+            );
+        }
+
+        return $registry;
+    }
 
     /**
      * @param class-string<ApplicationException> $exceptionClass
