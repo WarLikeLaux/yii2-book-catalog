abe2021da584d604163c0c75b7adbe14a31fc860
WarLikeLaux <warlikelaux080399@mail.ru>
2026-03-21 19:23:48 +0600

    #41 - refactored domain entities to use AuthorId VO: Book (int[] → AuthorId[]), Subscription, AuthorRepositoryInterface

--- src/domain/entities/Subscription.php
+++ src/domain/entities/Subscription.php
@@ -5,8 +5,7 @@ declare(strict_types=1);
 namespace app\domain\entities;
 
 use app\domain\common\IdentifiableEntityInterface;
-use app\domain\exceptions\DomainErrorCode;
-use app\domain\exceptions\ValidationException;
+use app\domain\values\AuthorId;
 use app\domain\values\Phone;
 
 final class Subscription implements IdentifiableEntityInterface
@@ -14,19 +13,16 @@ final class Subscription implements IdentifiableEntityInterface
     private function __construct(
         public private(set) ?int $id,
         public private(set) Phone $phone,
-        public private(set) int $authorId,
+        public private(set) AuthorId $authorId,
     ) {
-        if ($authorId <= 0) {
-            throw new ValidationException(DomainErrorCode::SubscriptionInvalidAuthorId);
-        }
     }
 
-    public static function create(Phone $phone, int $authorId): self
+    public static function create(Phone $phone, AuthorId $authorId): self
     {
         return new self(null, $phone, $authorId);
     }
 
-    public static function reconstitute(int $id, Phone $phone, int $authorId): self
+    public static function reconstitute(int $id, Phone $phone, AuthorId $authorId): self
     {
         return new self($id, $phone, $authorId);
     }
